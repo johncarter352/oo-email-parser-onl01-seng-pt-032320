e6cffa68f0bdc3ff86bb7fca7240bb2cb08c3715
John Carter <johncarter352@gmail.com>
2020-04-16 15:23:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,5 +5,10 @@ class EmailAddressParser
     @email = email
   end
     
+  def parse 
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
     
 end
